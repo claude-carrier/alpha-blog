e74a0e53aa8a54698494829def2ffe7fd9cc85e5
= <=>
2018-10-31 16:00:39 +0000

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,26 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+    
   def new
     @article = Article.new
   end
+  
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end 
   
   def create
     #render plain: params[:article].inspect
@@ -12,7 +31,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-      render 'new'
+      render 'edit'
     end
   end
   
