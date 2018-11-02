ee488a0881980e867dea94204b58122258493a64
= <=>
2018-11-02 14:42:58 +0000

    Add association between user and areticle and debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,8 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    # Next line is temporary fix
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
